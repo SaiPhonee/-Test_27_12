21ac7d5fa15108d445553cb637b624e2764db5db
SaiPhone <saiphone94@yandex.ru>
2021-12-27 20:46:10 +0300

    Test createArray

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,15 +4,27 @@
 
 int[] CreateArray(int size)
 {
-    int[] array=new int[size];
+    int[] array = new int[size];
     return array;
 }
 
 int[] FillArray(int[] array, int leftBound, int rightBound)
 {
-    for(int i=0; i<array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        array[i]= new Random().Next(leftBound, rightBound);
+        array[i] = new Random().Next(leftBound, rightBound);
     }
     return array;
 }
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+int[] array1 = CreateArray(8);
+FillArray(array1, 0, 10);
+PrintArray(array1);
