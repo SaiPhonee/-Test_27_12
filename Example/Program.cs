e7471503a3150ca72e7698f85fe50fc1f7e86927
SaiPhone <saiphone94@yandex.ru>
2021-12-27 20:42:38 +0300

    FillArray

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел.
+// Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+int[] CreateArray(int size)
+{
+    int[] array=new int[size];
+    return array;
+}
+
+int[] FillArray(int[] array, int leftBound, int rightBound)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        array[i]= new Random().Next(leftBound, rightBound);
+    }
+    return array;
+}
